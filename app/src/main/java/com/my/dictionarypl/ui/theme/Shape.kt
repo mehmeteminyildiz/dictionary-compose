4d97d6ffc780638988fcbc2e6d0fbb8a1b7fcbc6
Mehmet Yildiz <mhmtxyildiz@gmail.com>
2023-05-17 20:27:47 +0300

    gradle libraries have been added

---
+++ app/src/main/java/com/my/dictionarypl/ui/theme/Shape.kt
@@ -0,0 +1,15 @@
+package com.my.dictionarypl.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+/**
+created by Mehmet E. Yıldız
+ **/
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
